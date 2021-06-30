e28867fade1f474c789ee9df87896e5ea895e8e1
Mark1zzz <ermack228@yandex.ru>
2021-06-30 17:22:21 +0300

    изменение кода в связи с более конструктивным подходом

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 from plyer import notification
 import sqlite3
 import datetime
-import time
 import threading
 
-task = ''
 
+def check_tasks():
+    con = sqlite3.connect("scheduler_db.db")
+    cur = con.cursor()
 
-class Window:
-    def __init__(self):
-        pass
+    result = sorted(cur.execute("""SELECT * from jobs""").fetchall(), key=lambda x: x[0])
 
-    def check_tasks(self):
-        con = sqlite3.connect("scheduler_db.db")
-        cur = con.cursor()
-
-        result = sorted(cur.execute("""SELECT * from jobs""").fetchall(), key=lambda x: x[0])
-
-        return result
+    return result
 
 
 class Scheduler:
@@ -25,10 +18,9 @@ class Scheduler:
         self.list_of_tasks = list_of_tasks
 
     def start(self):
-        global task
         for i in range(len(self.list_of_tasks)):
             date_and_time = self.list_of_tasks[i][0]  # 27.06.2021 18:00 -> (2021, 6, 27, 18, 0, 0)
-            task_ = self.list_of_tasks[i][1]
+            task = self.list_of_tasks[i][1]
             time_now = datetime.datetime.now()
             time_now -= datetime.timedelta(0, 0, time_now.microsecond)
 
@@ -38,38 +30,14 @@ class Scheduler:
                                           int(date_and_time[11:13]),
                                           int(date_and_time[14:]))
             timedelta = task_time - time_now
-            time_in_sec = timedelta.seconds
-            task = task_
 
-            do_something_delayed = make_delayed(time_in_sec)(foo)
-            do_something_delayed()
+            threading.Timer(timedelta.total_seconds(), print_notification, [task]).start()
 
 
-def foo():
-    global task
+def print_notification(task):
     notification.notify("Новая задача начата", task, app_name="scheduler", timeout=10)
 
 
-def make_delayed(delay_in_seconds):
-    class CallDelayer:
-        def __init__(self):
-            self.call_event = threading.Event()
-            self.last_call_time = time.time()
-
-        def launch_with_delay(self, function):
-            def launched_with_delay(*args, **kwargs):
-                self.last_call_time += delay_in_seconds
-                wait_sec = self.last_call_time - time.time()
-                self.call_event.wait(wait_sec)
-                function(*args, **kwargs)
-
-            return launched_with_delay
-
-    call_delayer = CallDelayer()
-    return call_delayer.launch_with_delay
-
-
-win = Window()
-sched = Scheduler(win.check_tasks())
+sched = Scheduler(check_tasks())
 
 sched.start()
