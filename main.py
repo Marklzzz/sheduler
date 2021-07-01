5ab083b506f6108c0e5251e758323b66386530d5
Mark1zzz <ermack228@yandex.ru>
2021-07-01 22:04:21 +0300

    теперь в qt обёртке

--- main.py
+++ main.py
@@ -2,18 +2,92 @@ from plyer import notification
 import sqlite3
 import datetime
 import threading
+from PyQt5.QtWidgets import *
+import sys
 
 
 def check_tasks():
     con = sqlite3.connect("scheduler_db.db")
     cur = con.cursor()
-
-    result = sorted(cur.execute("""SELECT * from jobs""").fetchall(), key=lambda x: x[0])
+    result = list(sorted(cur.execute("""SELECT * from jobs""").fetchall(), key=lambda x: x[0]))
 
     return result
 
 
-class Scheduler:
+class Scheduler(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.con = sqlite3.connect('scheduler_db.db')
+        self.cur = self.con.cursor()
+
+        self.btn_add = QPushButton('Добавить', self)
+        self.count_of_notification = 0
+
+        self.list_qlines = []
+
+        self.initUI()
+
+    def initUI(self):
+        self.setFixedSize(400, 485)
+        self.move(300, 300)
+        self.setWindowTitle('Планировщик')
+
+        self.btn_add.resize(200, 30)
+        self.btn_add.clicked.connect(self.update_widget)
+
+        self.btn_save = QPushButton('Сохранить', self)
+        self.btn_save.resize(200, 30)
+        self.btn_save.clicked.connect(self.save)
+        self.draw()
+
+    def draw(self):
+        name = QLineEdit(self)
+        num = QLineEdit(self)
+        label1 = QLabel(self)
+        label2 = QLabel(self)
+
+        name.resize(150, 20)
+        num.resize(150, 20)
+
+        label1.setText('Время в формате <дд.мм.гггг чч:мм>')
+        label2.setText('Введите напоминание\nв произвольной форме')
+
+        label1.move(10, 10 + 100 * self.count_of_notification)
+        name.move(220, 10 + 100 * self.count_of_notification)
+        label2.move(10, 40 + 100 * self.count_of_notification)
+        num.move(220, 40 + 100 * self.count_of_notification)
+        self.btn_add.move(10, 75 + 100 * self.count_of_notification)
+        self.btn_save.move(10, 105 + 100 * self.count_of_notification)
+        self.list_qlines.append([name, num])
+
+        label1.show()
+        label2.show()
+        name.show()
+        num.show()
+
+    def update_widget(self):
+        self.count_of_notification += 1
+        self.draw()
+
+    def save(self):
+        for i in self.list_qlines:
+            if not i[0].text() or not i[1].text():
+                break
+        else:
+            for i in self.list_qlines:
+                self.update_db(str(i[0].text()), str(i[1].text()))
+        #self.hide()
+        self.con.close()
+        shed = START(check_tasks())
+        self.hide()
+        shed.start()
+
+    def update_db(self, time, job):
+        self.cur.execute('INSERT INTO jobs VALUES ("{}", "{}")'.format(str(time), str(job)))
+        self.con.commit()
+
+'''01.07.2021 20:07 пойти пить чай'''
+class START:
     def __init__(self, list_of_tasks):
         self.list_of_tasks = list_of_tasks
 
@@ -32,13 +106,16 @@ class Scheduler:
             timedelta = task_time - time_now
 
             threading.Timer(timedelta.total_seconds(), print_notification, [task]).start()
+        raise SystemExit(1)
+
 
 
 def print_notification(task):
     notification.notify("Новая задача начата", task, app_name="sheduler", timeout=10)
 
 
-
-sched = Scheduler(check_tasks())
-
-sched.start()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Scheduler()
+    ex.show()
+    sys.exit(app.exec_())
--- test.py
+++ test.py
@@ -1,39 +1,95 @@
-string = "abcdea fghebi baj kl f jbmn aopqjqc jekehj"
-import threading
-import time
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+
+import sqlite3
 
 
-def make_delayed(delay_in_seconds):
-    """
-    Decorator with parameter for making functions
-    launched with minimal delay between calls
-    """
+class Scheduler(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.btn_add = QPushButton('Добавить', self)
+        self.count_of_notification = 0
 
-    if delay_in_seconds <= 0:
-        raise ValueError("Non-positive delay: {}".format(delay_in_seconds))
+        self.list_qlines = []
 
-    class CallDelayer:
-        def __init__(self):
-            self.call_event = threading.Event()
-            self.last_call_time = time.time()
+        self.initUI()
 
-        def launch_with_delay(self, function):
-            def launched_with_delay(*args, **kwargs):
-                self.last_call_time += delay_in_seconds
-                function(*args, **kwargs)
-                wait_sec = self.last_call_time - time.time()
-                self.call_event.wait(wait_sec)
+    def initUI(self):
+        self.setFixedSize(400, 485)
+        self.move(300, 300)
+        self.setWindowTitle('Планировщик')
 
-            return launched_with_delay
+        self.btn_add.resize(200, 30)
+        self.btn_add.clicked.connect(self.update_widget)
 
-    call_delayer = CallDelayer()
-    return call_delayer.launch_with_delay
+        self.btn_save = QPushButton('Сохранить', self)
+        self.btn_save.resize(200, 30)
+        self.btn_save.clicked.connect(self.save)
+        self.draw()
+
+    def draw(self):
+        name = QLineEdit(self)
+        num = QLineEdit(self)
+        label1 = QLabel(self)
+        label2 = QLabel(self)
+
+        name.resize(150, 20)
+        num.resize(150, 20)
+
+        label1.setText('Время в формате <дд.мм.гггг чч:мм>')
+        label2.setText('Введите напоминание\nв произвольной форме')
+
+        label1.move(10, 10 + 100 * self.count_of_notification)
+        name.move(220, 10 + 100 * self.count_of_notification)
+        label2.move(10, 40 + 100 * self.count_of_notification)
+        num.move(220, 40 + 100 * self.count_of_notification)
+        self.btn_add.move(10, 75 + 100 * self.count_of_notification)
+        self.btn_save.move(10, 105 + 100 * self.count_of_notification)
+        self.list_qlines.append([name, num])
+
+        label1.show()
+        label2.show()
+        name.show()
+        num.show()
+
+    def update_widget(self):
+        self.count_of_notification += 1
+        self.draw()
+
+    def save(self):
+        for i in self.list_qlines:
+            if not i[0].text() or not i[1].text():
+                break
+        else:
+            for i in self.list_qlines:
+                self.update_db(i[0].text(), i[1].text())
+
+    def update_db(self, time, job):
+        con = sqlite3.connect('scheduler_db.db')
+        cur = con.cursor()
+        spisok = cur.execute('SELECT * FROM jobs WHERE time = {} AND task = {}'.format(time, job)).fetchall()
+        print(spisok)
+        if not spisok:
+            cur.execute('INSERT INTO jobs VALUES ({}, {})'.format(time, job))
+            con.commit()
 
 
-def foo():
-    print('aaa')
+
+class Example(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(500, 250)
+        self.scrollArea = QtWidgets.QScrollArea()
+        self.scrollArea.setFixedSize(500, 250)
+        self.setCentralWidget(self.scrollArea)
+        self.widget = Scheduler()
+        self.scrollArea.setWidget(self.widget)
+        self.show()
 
 
-do_something_delayed = make_delayed(1)(foo)
-for i in range(10):
-    do_something_delayed()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = Scheduler()
+    ex.show()
+    sys.exit(app.exec_())
